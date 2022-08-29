06b9aae300c58a3416f8639490db9eca91cde49d
Adrian Christian Santos <adrian.crsantos@gmail.com>
2022-08-29 19:57:01 +0800

    Changed they way of getting user input from useState to useRef

--- src/components/Users/UserForm.jsx
+++ src/components/Users/UserForm.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Error from '../UI/Error';
 
 import './UserForm.css';
 
 const UserForm = props => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState(false);
 
-  const usernameAddHandler = event => {
-    setEnteredUsername(event.target.value);
-  };
-  const ageAddHandler = event => {
-    setEnteredAge(event.target.value);
-  };
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  console.log(nameInputRef);
 
   const submitHandler = event => {
     event.preventDefault();
+
+    const enteredUsername = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
 
     if (enteredUsername.trim().length === 0) {
       setError({
@@ -39,8 +37,8 @@ const UserForm = props => {
       Age: enteredAge,
     };
     props.onSaveUserData(UserData);
-    setEnteredUsername('');
-    setEnteredAge('');
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
   const errorHandler = () => {
@@ -60,20 +58,11 @@ const UserForm = props => {
         <div className='container'>
           <div className='form-control'>
             <label>Username</label>
-            <input
-              type='text'
-              value={enteredUsername}
-              onChange={usernameAddHandler}
-            />
+            <input type='text' ref={nameInputRef} />
           </div>
           <div className='form-control'>
             <label>Age</label>
-            <input
-              type='number'
-              value={enteredAge}
-              onChange={ageAddHandler}
-              min='5'
-            />
+            <input type='number' ref={ageInputRef} />
           </div>
         </div>
         <div className='button-control'>
